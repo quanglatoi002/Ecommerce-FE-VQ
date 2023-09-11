ba1a58c0a453a336ed17b68334782568b9421a21
quang <quanglatoi002@gmail.com>
2023-09-11 19:22:52 +0700

    update coding

---
+++ src/components/Footer.js
@@ -0,0 +1,118 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="py-4">
+                <div className="container-xxl">
+                    <div className="row align-items-center">
+                        <div className="col-5">
+                            <div className="footer-top-data d-flex gap-30 align-items-center">
+                                <img
+                                    src="images/newsletter.png"
+                                    alt="newsletter"
+                                />
+                                <h2 className="mb-0 text-white">
+                                    Sign Up for NewsLetter
+                                </h2>
+                            </div>
+                        </div>
+                        <div className="col-7">
+                            <div class="input-group">
+                                <input
+                                    type="text"
+                                    className="form-control py-2"
+                                    placeholder="Your Email Address"
+                                    aria-label="Your Email Address"
+                                    aria-describedby="basic-addon2"
+                                />
+                                <span
+                                    className="input-group-text p-3"
+                                    id="basic-addon2"
+                                >
+                                    Subscribe
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <footer className="py-4">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-4">
+                            <h4 className="text-white mb-4">Contact Us</h4>
+                            <div></div>
+                        </div>
+                        <div className="col-3">
+                            <h4 className="text-white mb-4">Information</h4>
+                            <div className="footer-links d-flex flex-column">
+                                <Link className="text-white py-2 mb-1">
+                                    Privacy Policy
+                                </Link>
+                                <Link className="text-white py-2 mb-1">
+                                    Refund Policy
+                                </Link>
+                                <Link className="text-white py-2 mb-1">
+                                    Shipping Policy
+                                </Link>
+                                <Link className="text-white py-2 mb-1">
+                                    Terms & Conditions
+                                </Link>
+                                <Link className="text-white py-2 mb-1">
+                                    Blogs
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <h4 className="text-white mb-4">Account</h4>
+                            <div className="footer-links d-flex flex-column">
+                                <Link className="text-white py-2 mb-1">
+                                    About Us
+                                </Link>
+                                <Link className="text-white py-2 mb-1">
+                                    Faq
+                                </Link>
+                                <Link className="text-white py-2 mb-1">
+                                    Contact
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <h4 className="text-white mb-4">Quick Links</h4>
+                            <div className="footer-links d-flex flex-column">
+                                <Link className="text-white py-2 mb-1">
+                                    Laptops
+                                </Link>
+                                <Link className="text-white py-2 mb-1">
+                                    Headphones
+                                </Link>
+                                <Link className="text-white py-2 mb-1">
+                                    Tablets
+                                </Link>
+                                <Link className="text-white py-2 mb-1">
+                                    Watch
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <footer className="py-4">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <p className="text-center mb-0 text-white">
+                                &copy: {new Date().getFullYear()}; Powered by
+                                Developer
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
