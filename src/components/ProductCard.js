fe4c59af2ded2fa95f8bfd6eceee0ea648fce72a
quang <quanglatoi002@gmail.com>
2023-09-13 20:10:02 +0700

    updating coding(3)

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,62 +1,150 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+    const { grid } = props;
+    let location = useLocation();
+    console.log(location);
     return (
-        <div className="col-3">
-            <Link className="product-card position-relative">
-                <div className="wishlist-icon position-absolute">
-                    <Link>
-                        <img src="images/wish.svg" alt="wishlist" />
-                    </Link>
-                </div>
-                <div className="product-image">
-                    <img
-                        className="img-fluid"
-                        src="images/watch.jpg"
-                        alt="product images"
-                    />
-                    <img
-                        className="img-fluid"
-                        src="images/watch-1.webp"
-                        alt="product images"
-                    />
-                </div>
-
-                <div className="product-details">
-                    <h6 className="brand">Havel</h6>
-                    <h5 className="product-title">
-                        Kids headphones bulk 10 pack multi colored for student
-                    </h5>
-                    <ReactStars
-                        count={5}
-                        size={24}
-                        value={3}
-                        edit={false}
-                        activeColor="#ffd700"
-                    />
-                    <p className="price">$100.00</p>
-                </div>
-                <div className="action-bar position-absolute">
-                    <div className="d-flex flex-column gap-15">
+        <>
+            <div
+                className={`${
+                    location.pathname === "/store" ? `gr-${grid}` : "col-3"
+                }`}
+            >
+                <Link className="product-card position-relative">
+                    <div className="wishlist-icon position-absolute">
                         <Link>
-                            <img
-                                src="images/prodcompare.svg"
-                                alt="prodcompare"
-                            />
-                        </Link>
-                        <Link>
-                            <img src="images/view.svg" alt="view" />
-                        </Link>
-
-                        <Link>
-                            <img src="images/add-cart.svg" alt="addCart" />
+                            <img src="images/wish.svg" alt="wishlist" />
                         </Link>
                     </div>
-                </div>
-            </Link>
-        </div>
+                    <div className="product-image">
+                        <img
+                            className="img-fluid"
+                            src="images/watch.jpg"
+                            alt="product images"
+                        />
+                        <img
+                            className="img-fluid"
+                            src="images/watch-1.webp"
+                            alt="product images"
+                        />
+                    </div>
+
+                    <div className="product-details">
+                        <h6 className="brand">Havel</h6>
+                        <h5 className="product-title">
+                            Kids headphones bulk 10 pack multi colored for
+                            student
+                        </h5>
+                        <ReactStars
+                            count={5}
+                            size={24}
+                            value={3}
+                            edit={false}
+                            activeColor="#ffd700"
+                        />
+                        <p
+                            className={`description ${
+                                grid === 12 ? "d-block" : "d-none"
+                            }`}
+                        >
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. A perspiciatis, aspernatur culpa quis fuga
+                            adipisci repellendus deserunt molestias vero tenetur
+                            rem.
+                        </p>
+                        <p className="price">$100.00</p>
+                    </div>
+                    <div className="action-bar position-absolute">
+                        <div className="d-flex flex-column gap-15">
+                            <Link>
+                                <img
+                                    src="images/prodcompare.svg"
+                                    alt="prodcompare"
+                                />
+                            </Link>
+                            <Link>
+                                <img src="images/view.svg" alt="view" />
+                            </Link>
+
+                            <Link>
+                                <img src="images/add-cart.svg" alt="addCart" />
+                            </Link>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+            <div
+                className={`${
+                    location.pathname === "/store" ? `gr-${grid}` : "col-3"
+                }`}
+            >
+                <Link className="product-card position-relative">
+                    <div className="wishlist-icon position-absolute">
+                        <Link>
+                            <img src="images/wish.svg" alt="wishlist" />
+                        </Link>
+                    </div>
+                    <div className="product-image">
+                        <img
+                            className="img-fluid"
+                            src="images/watch.jpg"
+                            alt="product images"
+                        />
+                        <img
+                            className="img-fluid"
+                            src="images/watch-1.webp"
+                            alt="product images"
+                        />
+                    </div>
+
+                    <div className="product-details">
+                        <h6 className="brand">Havel</h6>
+                        <h5 className="product-title">
+                            Kids headphones bulk 10 pack multi colored for
+                            student
+                        </h5>
+                        <ReactStars
+                            count={5}
+                            size={24}
+                            value={3}
+                            edit={false}
+                            activeColor="#ffd700"
+                        />
+                        <p
+                            className={`description ${
+                                grid === 12 ? "d-block" : "d-none"
+                            }`}
+                        >
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. A perspiciatis, aspernatur culpa quis fuga
+                            adipisci repellendus deserunt molestias vero tenetur
+                            rem.
+                        </p>
+                        <p className="price">$100.00</p>
+                    </div>
+                    <div className="action-bar position-absolute">
+                        <div className="d-flex flex-column gap-15">
+                            <Link>
+                                <img
+                                    src="images/prodcompare.svg"
+                                    alt="prodcompare"
+                                />
+                            </Link>
+                            <Link>
+                                <img src="images/view.svg" alt="view" />
+                            </Link>
+
+                            <Link>
+                                <img src="images/add-cart.svg" alt="addCart" />
+                            </Link>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        </>
     );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -252,6 +252,98 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+            <section className="famous-wrapper py-5 home-wrapper-2">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-3">
+                            <div
+                                className="famous-card 
+                             position-relative"
+                            >
+                                <img
+                                    src="images/famous-2.jpeg"
+                                    alt="famous"
+                                    className="img-fluid"
+                                />
+                                <div className="famous-content position-absolute">
+                                    <h5>Big Screen</h5>
+                                    <h6>Smart Watch Series 9</h6>
+                                    <p>From $399 or $16.6/mo. for 24mo</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div
+                                className="famous-card 
+                             position-relative"
+                            >
+                                <img
+                                    src="images/famous-1.webp"
+                                    alt="famous"
+                                    className="img-fluid"
+                                />
+                                <div className="famous-content position-absolute">
+                                    <h5 className="text-dark">
+                                        Studio Display
+                                    </h5>
+                                    <h6 className="text-dark">
+                                        Smart Watch Series 9
+                                    </h6>
+                                    <p className="text-dark">
+                                        From $399 or $16.6/mo. for 24mo
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div
+                                className="famous-card 
+                             position-relative"
+                            >
+                                <img
+                                    src="images/famous-1.webp"
+                                    alt="famous"
+                                    className="img-fluid"
+                                />
+                                <div className="famous-content position-absolute">
+                                    <h5 className="text-dark">
+                                        Studio Display
+                                    </h5>
+                                    <h6 className="text-dark">
+                                        Smart Watch Series 9
+                                    </h6>
+                                    <p className="text-dark">
+                                        From $399 or $16.6/mo. for 24mo
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div
+                                className="famous-card 
+                             position-relative"
+                            >
+                                <img
+                                    src="images/famous-1.webp"
+                                    alt="famous"
+                                    className="img-fluid"
+                                />
+                                <div className="famous-content position-absolute">
+                                    <h5 className="text-dark">
+                                        Studio Display
+                                    </h5>
+                                    <h6 className="text-dark">
+                                        Smart Watch Series 9
+                                    </h6>
+                                    <p className="text-dark">
+                                        From $399 or $16.6/mo. for 24mo
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <section className="special-wrapper py-5 home-wrapper-2">
                 <div className="container-xxl">
                     <div className="row">
@@ -265,6 +357,24 @@ const Home = () => {
                         <SpecialProduct />
                         <SpecialProduct />
                         <SpecialProduct />
+                        <SpecialProduct />
+                    </div>
+                </div>
+            </section>
+            <section className="popular-wrapper py-5 home-wrapper-2">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <h3 className="section-heading">
+                                Our Popular Products
+                            </h3>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
                     </div>
                 </div>
             </section>
